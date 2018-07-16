1191383c63259a9ba223034c04148d3e40ad078c
kongxingongzi <1543892875@qq.com>
2018-07-16 16:52:10 +0800

    ffc

--- weixin.config.php
+++ weixin.config.php
@@ -1,5 +1,5 @@
 <?php
-define('APPID','APPID');
-define('APPSECRET','APPSECRET');
-define('TOKEN','Token');
+define('APPID','wx54f0e94fbee136c0');
+define('APPSECRET','6ec9e287b58a6a06ef8cb04c604e9113');
+define('TOKEN','weixin');
 ?>
